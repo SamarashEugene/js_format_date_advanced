842de0f8d8cc001668a9b4ea86a61ce550d9c4e0
Samarash Eugene <samarasha95@gmail.com>
2025-01-31 23:24:29 +0200

    formatDate function is implemented

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,36 @@
  *
  * @returns {string}
  */
+
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateSeparated = date.split(fromFormat[3]);
+  const oldForm = {
+    [fromFormat[0]]: dateSeparated[0],
+    [fromFormat[1]]: dateSeparated[1],
+    [fromFormat[2]]: dateSeparated[2],
+  };
+
+  const newForm = {};
+  let century = '';
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    if (fromFormat[i] === 'YY' && toFormat.includes('YYYY')) {
+      century = dateSeparated[i] < 30 ? 20 : 19;
+      newForm[toFormat[i]] = century + dateSeparated[i];
+    } else if (fromFormat[i] === 'YYYY' && toFormat.includes('YY')) {
+      newForm[toFormat[i]] = dateSeparated[i].slice(-2);
+    } else {
+      newForm[toFormat[i]] = oldForm[toFormat[i]];
+    }
+  }
+
+  const newDateArr = [];
+
+  for (const i in newForm) {
+    newDateArr.push(newForm[i]);
+  }
+
+  return newDateArr.join(toFormat[3]);
 }
 
 module.exports = formatDate;
