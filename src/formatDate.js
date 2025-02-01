cb33bb6adc6154c01c8c1fe113ba42f10a6497c9
Samarash Eugene <samarasha95@gmail.com>
2025-02-01 14:26:22 +0200

    formatDate function is implemente

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,22 +9,38 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  const dateSeparated = date.split(fromFormat[3]);
-  const oldForm = {
-    [fromFormat[0]]: dateSeparated[0],
-    [fromFormat[1]]: dateSeparated[1],
-    [fromFormat[2]]: dateSeparated[2],
-  };
+  let separator = '';
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    if (fromFormat[i].length === 1) {
+      separator = fromFormat[i];
+      break;
+    }
+  }
+
+  const dateSeparated = date.split(separator);
+
+  const oldForm = {};
+  let index = 0;
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    if (fromFormat[i].length > 1) {
+      oldForm[fromFormat[i]] = dateSeparated[index++];
+    }
+  }
 
   const newForm = {};
-  let century = '';
 
   for (let i = 0; i < toFormat.length - 1; i++) {
-    if (fromFormat[i] === 'YY' && toFormat.includes('YYYY')) {
-      century = dateSeparated[i] < 30 ? 20 : 19;
-      newForm[toFormat[i]] = century + dateSeparated[i];
-    } else if (fromFormat[i] === 'YYYY' && toFormat.includes('YY')) {
-      newForm[toFormat[i]] = dateSeparated[i].slice(-2);
+    if (toFormat[i].length === 1) {
+      continue;
+    }
+
+    if (toFormat[i] === 'YYYY' && oldForm['YY']) {
+      newForm[toFormat[i]] =
+        (parseInt(oldForm['YY']) < 30 ? '20' : '19') + oldForm['YY'];
+    } else if (toFormat[i] === 'YY' && oldForm['YYYY']) {
+      newForm[toFormat[i]] = oldForm['YYYY'].slice(-2);
     } else {
       newForm[toFormat[i]] = oldForm[toFormat[i]];
     }
@@ -32,11 +48,24 @@ function formatDate(date, fromFormat, toFormat) {
 
   const newDateArr = [];
 
-  for (const i in newForm) {
-    newDateArr.push(newForm[i]);
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    if (toFormat[i].length > 1) {
+      newDateArr.push(newForm[toFormat[i]]);
+    } else {
+      newDateArr.push(toFormat[i]);
+    }
   }
 
-  return newDateArr.join(toFormat[3]);
+  let newSeparator = '';
+
+  for (let i = 0; i < toFormat.length; i++) {
+    if (toFormat[i].length === 1) {
+      newSeparator = toFormat[i];
+      break;
+    }
+  }
+
+  return newDateArr.join(newSeparator);
 }
 
 module.exports = formatDate;
